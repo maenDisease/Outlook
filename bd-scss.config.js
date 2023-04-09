b19b5bdd1972ba6a764cdcac85ff01963d636b41
Patrick Jr <patrickcaulton423@gmail.com>
2023-04-09 09:00:12 +0100

    Update bd-scss.config.js
    Lowercase o for the url as it was leading to a broken page.

--- bd-scss.config.js
+++ bd-scss.config.js
@@ -4,7 +4,7 @@ export default {
 		name: "Outlook",
 		author: "maenDisease",
 		version: "1.0.0",
-		description: "Based on Outlook Web. Customize with https://bdeditor.dev/theme/Outlook",
+		description: "Based on Outlook Web. Customize with https://bdeditor.dev/theme/outlook",
 		source: "https://github.com/maenDisease/Outlook",
 		website: "https://maendisease.github.io/",
 		invite: "BShu37e4jg"
